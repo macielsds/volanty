b052aaa84e98d9222ae01a132322030173a5f3bd
Vinicius Rocha <macielsds@gmail.com>
2019-10-28 11:43:26 -0300

    Updates

---
+++ precache-manifest.8f92bc88d7e7eb8f8f5fd56740a999a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f61809e64d2564efde80a4a48d2a831f",
+    "url": "/volanty/desafio/index.html"
+  },
+  {
+    "revision": "0736f8742fbe67655257",
+    "url": "/volanty/desafio/static/css/main.9332e802.chunk.css"
+  },
+  {
+    "revision": "ea23e97e5751f59753e7",
+    "url": "/volanty/desafio/static/js/2.24c924ed.chunk.js"
+  },
+  {
+    "revision": "0736f8742fbe67655257",
+    "url": "/volanty/desafio/static/js/main.8150df6c.chunk.js"
+  },
+  {
+    "revision": "415cd331c3e7ee346eb3",
+    "url": "/volanty/desafio/static/js/runtime-main.5020354f.js"
+  },
+  {
+    "revision": "cdfdf297de0b59cd19d20e7a1e5fc381",
+    "url": "/volanty/desafio/static/media/arrow.cdfdf297.svg"
+  }
+]);
