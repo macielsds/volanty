adb1d7bed9f52eee6341d1d05b0356bd7b7ab5c6
Vinicius Rocha <macielsds@gmail.com>
2019-10-30 17:39:05 -0300

    Reorganizando repositório

---
+++ build/precache-manifest.a2205eaf8766b69ca4fb9eecd9fcd8aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "565af3cec382459a1f1a44b37de12dc3",
+    "url": "/volanty/index.html"
+  },
+  {
+    "revision": "a36f37afa2fa7244b5a0",
+    "url": "/volanty/static/css/main.9332e802.chunk.css"
+  },
+  {
+    "revision": "19d0b55fdd5290b29e22",
+    "url": "/volanty/static/js/2.0dc2af67.chunk.js"
+  },
+  {
+    "revision": "a36f37afa2fa7244b5a0",
+    "url": "/volanty/static/js/main.76aa8fa0.chunk.js"
+  },
+  {
+    "revision": "708ad7ff6536fb837b0c",
+    "url": "/volanty/static/js/runtime-main.60ed0ef7.js"
+  },
+  {
+    "revision": "cdfdf297de0b59cd19d20e7a1e5fc381",
+    "url": "/volanty/static/media/arrow.cdfdf297.svg"
+  }
+]);
